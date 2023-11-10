81336ab57a14199bcd6ed3212c1490617cdfd0e9
Navaneeth V <navaneethnair69@gmail.com>
2023-11-10 13:57:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}>
+      <h1 className='bg-red-200'>Tailwind css</h1>
+    </div>
+  )
+}
+
+export default App
