7f35a38bc2ade6b76ad32a44e189562903d682ae
Navaneeth V <navaneethnair69@gmail.com>
2024-01-19 13:38:23 +0530

    first commit

--- src/components/inputBox.jsx
+++ src/components/inputBox.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, {useId} from 'react'
 
-function inputBox() {
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) {
+
+  const id = useId()
   return (
-    <div>inputBox</div>
+
+    <div  className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className='w-1-2'>
+        <label htmlFor={id} className='text-black/40 mb-2 inline-block'>{label}</label>
+        <input
+        id={id}
+        type="number" 
+        className='outline-none w-full bg-transparent py-1.5'
+        placeholder='Amount'
+        disabled={amountDisabled}
+        value={amount}
+        onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} 
+          //to convert the fetched string in to a number
+        />
+      </div>
+
+      <div className='w-1/2 flex flex-wrap justify-end text-right'>
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select 
+        className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+        value={selectedCurrency}
+        onChange={(e) => onCurrencyChange && onAmountChange(Number(e.target.value))} 
+          
+          disabled={currencyDisabled}
+        >
+            {currencyOptions.map((currency) => (
+              <option key={currency} value={currency}>{currency}</option>
+            ))}
+        </select>
+      </div>
+      
+    </div>
   )
 }
 
-export default inputBox
+export default InputBox
